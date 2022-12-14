909cff932828101d6187660e4dcb12f6e14472fd
dotuananh <dotuananh220201@gmail.com>
2022-12-14 13:34:11 +0700

    Update releases page

--- src/components/Releases/index.tsx
+++ src/components/Releases/index.tsx
@@ -7,6 +7,9 @@ import { BiChevronDown } from "react-icons/bi";
 import { FiSearch } from "react-icons/fi";
 import { GrFormNext, GrFormPrevious } from "react-icons/gr";
 import { MdNavigateNext, MdOutlineNavigateBefore } from "react-icons/md";
+import { IoMusicalNoteOutline } from "react-icons/io5";
+import Single from "components/Common/Single";
+import DemoImage from "assets/images/demo-image.jpg";
 
 const Releases = () => {
   return (
@@ -69,6 +72,78 @@ const Releases = () => {
             <Music />
           </div>
           <Pagination />
+        </div>
+      </div>
+      <div className="mt-6">
+        <div className="flex flex-row gap-3 items-center text-3xl mb-6">
+          <IoMusicalNoteOutline className="text-[#25A56A] mt-1.5" />
+          <span className="text-white">New singles</span>
+        </div>
+        <div className="flex flex-row gap-12">
+          <div>
+            <Single
+              image={DemoImage}
+              songName="Chúng ta của hiện tại"
+              singerName="Sơn Tùng MTP"
+              songTime="5:01"
+            />
+            <Single
+              image={DemoImage}
+              songName="Chúng ta của hiện tại"
+              singerName="Sơn Tùng MTP"
+              songTime="5:01"
+            />
+            <Single
+              image={DemoImage}
+              songName="Chúng ta của hiện tại"
+              singerName="Sơn Tùng MTP"
+              songTime="5:01"
+            />
+            <Single
+              image={DemoImage}
+              songName="Chúng ta của hiện tại"
+              singerName="Sơn Tùng MTP"
+              songTime="5:01"
+            />
+            <Single
+              image={DemoImage}
+              songName="Chúng ta của hiện tại"
+              singerName="Sơn Tùng MTP"
+              songTime="5:01"
+            />
+          </div>
+          <div>
+            <Single
+              image={DemoImage}
+              songName="Chúng ta của hiện tại"
+              singerName="Sơn Tùng MTP"
+              songTime="5:01"
+            />
+            <Single
+              image={DemoImage}
+              songName="Chúng ta của hiện tại"
+              singerName="Sơn Tùng MTP"
+              songTime="5:01"
+            />
+            <Single
+              image={DemoImage}
+              songName="Chúng ta của hiện tại"
+              singerName="Sơn Tùng MTP"
+              songTime="5:01"
+            />
+            <Single
+              image={DemoImage}
+              songName="Chúng ta của hiện tại"
+              singerName="Sơn Tùng MTP"
+              songTime="5:01"
+            />
+            <Single
+              image={DemoImage}
+              songName="Chúng ta của hiện tại"
+              singerName="Sơn Tùng MTP"
+              songTime="5:01"
+            />
+          </div>
         </div>
       </div>
     </div>
