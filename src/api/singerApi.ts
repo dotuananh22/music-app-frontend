ea7d86ba8ffdb74edb5f89867099d13d64b4e5a0
Pham Quoc An <19521187@gm.uit.edu.vn>
2023-03-04 11:41:12 +0700

    Implement Search User Api

--- src/api/singerApi.ts
+++ src/api/singerApi.ts
@@ -1,5 +1,6 @@
 import ApiResponse from "types/ApiResponse";
 import QueryInput from "types/QueryInput";
+import SearchQuery from "types/SearchQuery";
 import GetAllSingerResponse from "types/singer/GetAllSingerResponse";
 import Singer from "types/singer/Singer";
 import axiosClient from "./axiosClient";
@@ -14,12 +15,23 @@ const getAllSingers = async (
   );
 };
 
+const searchSingers = async (
+  query: SearchQuery<Singer>
+): Promise<ApiResponse<Singer[]>> => {
+  return await axiosClient.get(
+    `/singer/search?query=${query.query}&limit=${
+      query.limit
+    }&sort=${JSON.stringify(query.sort)}&order=${JSON.stringify(query.order)}`
+  );
+};
+
 const getOneSinger = async (id: string): Promise<ApiResponse<Singer>> => {
   return await axiosClient.get(`/singer/${id}`);
 };
 
 const singerApi = {
   getAllSingers,
+  searchSingers,
   getOneSinger,
 };
 
